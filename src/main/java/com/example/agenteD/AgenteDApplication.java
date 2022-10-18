ef92a82c50a96c7dcf0fec049ffca02ddf04793f
JAC-jr <113381971+JAC-jr@users.noreply.github.com>
2022-10-18 16:43:04 +0100

    first commit
    multi thread for test functionality of  components in a sistem

---
+++ src/main/java/com/example/agenteD/AgenteDApplication.java
@@ -0,0 +1,13 @@
+package com.example.agenteD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agenteD/AgenteDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agenteD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
