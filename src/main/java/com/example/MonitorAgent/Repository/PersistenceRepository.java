553748bc16b1afd3628e1537ba2fe63123733f39
jseijas98 <92530957+jseijas98@users.noreply.github.com>
2022-11-17 09:26:31 -0400

    optional to lsit

--- src/main/java/com/example/MonitorAgent/Repository/PersistenceRepository.java
+++ src/main/java/com/example/MonitorAgent/Repository/PersistenceRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PersistenceRepository extends JpaRepository<Persistence, Integer> {
-    Optional<Persistence> findByApplicationId(Integer applicationId);
+    List<Persistence> findByApplicationId(Integer applicationId);
 }
--- src/main/java/com/example/MonitorAgent/Repository/ServiceRepository.java
+++ src/main/java/com/example/MonitorAgent/Repository/ServiceRepository.java
@@ -2,13 +2,14 @@ package com.example.MonitorAgent.Repository;
 
 
 import com.example.MonitorAgent.Entity.Service;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
-
-    Optional<Service> findBypplicationId(Integer applicationId);
+    List <Service> findBypplicationId(Integer applicationId);
 }
