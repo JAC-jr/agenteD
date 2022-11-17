1205572ad65c928ba4d15781e4d769e14d9d8cfe
jseijas98 <92530957+jseijas98@users.noreply.github.com>
2022-11-17 09:27:16 -0400

    A

--- src/main/java/com/example/MonitorAgent/Repository/ServiceRepository.java
+++ src/main/java/com/example/MonitorAgent/Repository/ServiceRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
-    List <Service> findBypplicationId(Integer applicationId);
+    List <Service> findByApplicationId(Integer applicationId);
 }
