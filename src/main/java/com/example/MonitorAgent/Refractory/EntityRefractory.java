e1e85558be7603ff40b8e990ef62d21448d11d4e
JAC-jr <113381971+JAC-jr@users.noreply.github.com>
2022-11-17 13:12:00 +0000

    COMMIT

--- src/main/java/com/example/MonitorAgent/Refractory/EntityRefractory.java
+++ src/main/java/com/example/MonitorAgent/Refractory/EntityRefractory.java
@@ -41,7 +41,7 @@ public class EntityRefractory {
         Thread.sleep(1000);
         result.forEach(application -> {
             logger.info("{}", application);
-            objetRefractory.objetResponse(application);
+           // objetRefractory.objetResponse(application);
         });
         return CompletableFuture.completedFuture(result);
     }
@@ -53,7 +53,7 @@ public class EntityRefractory {
 
         result.forEach(integration -> {
             logger.info("{}", integration);
-            objetRefractory.objetResponse(integration);
+           // objetRefractory.objetResponse(integration);
         });
         return CompletableFuture.completedFuture(result);
     }
@@ -65,7 +65,7 @@ public class EntityRefractory {
 
         result.forEach(loadBalancer -> {
             logger.info("{}", loadBalancer);
-            objetRefractory.objetResponse(loadBalancer);
+           // objetRefractory.objetResponse(loadBalancer);
         });
         return CompletableFuture.completedFuture(result);
     }
@@ -77,7 +77,7 @@ public class EntityRefractory {
 
         result.forEach(persistence -> {
             logger.info("{}", persistence);
-            objetRefractory.objetResponse(persistence);
+           // objetRefractory.objetResponse(persistence);
         });
         return CompletableFuture.completedFuture(result);
     }
@@ -89,7 +89,7 @@ public class EntityRefractory {
 
         result.forEach(service -> {
             logger.info("{}", service);
-            objetRefractory.objetResponse(service);
+           // objetRefractory.objetResponse(service);
         });
         return CompletableFuture.completedFuture(result);
     }
